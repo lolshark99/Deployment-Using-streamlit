9f7e1077352ddba5bce966b101e577414c45e842
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-04-20 00:14:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -23,8 +23,9 @@ transform = transforms.Compose([
     transforms.Normalize(mean_list_train, std_list_train)
 ])
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 cap = cv2.VideoCapture(0)
 
 
